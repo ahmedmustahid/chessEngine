aa5328aa9c375c47a5e7f671b68b2515fbfd1440
ahmedmustahid <einsteinium25@gmail.com>
2024-11-15 09:25:01 +0600

    move pieces

--- chess/chessEngine.py
+++ chess/chessEngine.py
@@ -1,12 +1,30 @@
-class ChessState:
+class GameState:
     def __init__(self):
         self.board = [
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bp","bp","bp","bp","bp","bp","bp","bp",],
-            [" "," "," "," "," "," "," "," ",],
-            [" "," "," "," "," "," "," "," ",],
-            [" "," "," "," "," "," "," "," ",],
-            [" "," "," "," "," "," "," "," ",],
+            ["--","--","--","--","--","--","--","--",],
+            ["--","--","--","--","--","--","--","--",],
+            ["--","--","--","--","--","--","--","--",],
+            ["--","--","--","--","--","--","--","--",],
             ["wp", "wp","wp","wp","wp","wp","wp","wp",],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+
+        self.whiteToMove = True
+        self.movelog = []
+
+class Move:
+
+    ranksToRows = {"1":"7", "2":"6", "3":"5", "4":"4", "5":"3", "6":"2", "7":"1", "8":"0"}
+    rowsToRanks = {v:k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0,"b": 1,"c": 2,"d": 3,"e": 4,"f": 5,"g": 6,"h": 7}
+    colsToFiles = {v:k for k,v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
