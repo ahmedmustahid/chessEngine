70ccc775c39d01f95210aa41a62ea3e48ad6daf3
ahmedmustahid <einsteinium25@gmail.com>
2024-12-11 07:28:52 +0600

    added black pawn moves

--- chess/chessEngine.py
+++ chess/chessEngine.py
@@ -98,6 +98,19 @@ class GameState:
             if c + 1 <= 7:
                 if self.board[r-1][c+1][0]=="b":
                     moves.append(Move((r,c), (r-1, c+1), self.board))
+        
+        else:
+            if self.board[r+1][c]=="--":
+                moves.append(Move((r,c), (r+1, c), self.board))
+                if r==1 and self.board[r+2][c]=="--":
+                    moves.append(Move((r,c), (r+2, c), self.board))
+            if c - 1 >=0:
+                if self.board[r+1][c-1][0]=="w":
+                    moves.append(Move((r,c),(r+1, c-1), self.board))
+            if c + 1 <= 7:
+                if self.board[r+1][c+1][0]=="w":
+                    moves.append(Move((r,c),(r+1, c+1), self.board))
+
 
         
     def getRookMoves(self, r, c, moves):
